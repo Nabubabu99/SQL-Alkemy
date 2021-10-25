845d70af3889f9bebca88cb084a6c304b13d3368
Nabubabu99 <53090779+Nabubabu99@users.noreply.github.com>
2021-10-25 13:02:20 -0300

    Update punto 3.sql

--- SQLII-EjFinal/punto 3.sql
+++ SQLII-EjFinal/punto 3.sql
@@ -1,4 +1,2 @@
-SELECT DISTINCT e.legajo, (SELECT count(estudiante_legajo)
-                                     from INSCRIPCION WHERE estudiante_legajo = e.legajo) as 'cursos realizados'
-from ESTUDIANTE e INNER JOIN INSCRIPCION i on e.legajo = i.estudiante_legajo
-WHERE (SELECT count(estudiante_legajo) from INSCRIPCION WHERE estudiante_legajo = e.legajo) = 0;
+SELECT * from ESTUDIANTE e WHERE (SELECT count(estudiante_legajo)
+                                  from INSCRIPCION WHERE estudiante_legajo = e.legajo) = 0;
